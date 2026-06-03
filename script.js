5f91c88d26b3d62710cac0718a6f860304595225
saqibbhatti <sqb_bhatti@yahoo.com>
2026-06-03 21:23:34 +0800

    New Recipe App has been added

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+
+
+const recipeImages = [
+"./assets/GroceryList.png",
+"./assets/Add.png"
+];
+
+let currentRecipeIndex = 0;
+
+function showRecipeImage() {
+  document.getElementById("recipe-slider-image").src =
+  recipeImages[currentRecipeIndex];
+}
+
+window.nextSlide = function () {
+  currentRecipeIndex =
+  (currentRecipeIndex + 1) % recipeImages.length;
+
+  console.log("Current Index:", currentRecipeIndex);
+
+  showRecipeImage();
+}
+
+window.prevSlide = function () {
+  currentRecipeIndex =
+  (currentRecipeIndex - 1 + recipeImages.length) %
+  recipeImages.length;
+
+  showRecipeImage();
+}
+
+
+
+
+
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+console.log("Script Loaded");
